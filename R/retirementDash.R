d852b959b245dbda9d9d4b2a4205ac168df2150c
RobWiederstein <khuon68@gmail.com>
2021-09-30 09:39:29 -0500

    first commit

---
+++ R/retirementDash.R
@@ -0,0 +1,18 @@
+#' retirementDash:  Finding retirement locations in the US.
+#'
+#' The retirementDash package can provide and narrow prospective retirement counties by important
+#' demographic, environmental, housing affordability and other characteristics.
+#'
+#' @author
+#'
+#' Rob Wiederstein
+#'
+#' @seealso
+#'
+#' Useful links:
+#' - https://github.com/robwiederstein/retirementDash/
+#' - Report bugs at https://github.com/robwiederstein/retirementDash/issues
+#' @docType package
+#' @name retirementDash
+NULL
+#> NULL
