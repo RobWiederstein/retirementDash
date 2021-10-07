76ddfc4fa4b29f35bd449fdf0d65660c48132adf
RobWiederstein <khuon68@gmail.com>
2021-10-06 19:18:40 -0500

    add missing globals

--- R/globals.R
+++ R/globals.R
@@ -5,4 +5,4 @@ utils::globalVariables(c("average_daily_pm2_5", "avg_annual_temp", "broadband_20
                          "med_hh_inc_2019", "median_home_price", "partisan_lean", "pct_bachelor",
                          "pct_pop_change", "pop_2020", "prim_care_dr_rate", "rucc_2013", "state",
                          "state_origin", "state_target", "value", "violent_crime_rate", "years_to_payoff",
-                         "yoy_price_chg_pct"))
+                         "yoy_price_chg_pct", "county", "fips", "lat", "lon"))
